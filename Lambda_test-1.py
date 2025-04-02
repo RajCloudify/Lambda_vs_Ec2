d7b57a05762d7446074c87798cbe103c44ba24ab
Raj Attingal <rajattingal1@gmail.com>
2025-04-02 14:28:01 +0400

    Create Lambda_test-1.py

---
+++ Lambda_test-1.py
@@ -0,0 +1,16 @@
+import time
+
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+def lambda_handler(event, context):
+    start = time.time()
+    result = fib(30)
+    end = time.time()
+
+    return {
+        'result': result,
+        'time_sec': end - start
+    }
